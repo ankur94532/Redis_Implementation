225101f262f4810c8b0c4f0be7ce719544ae3ce0
Shashwat Tripathi <shashwattrpth96@gmail.com>
2025-09-30 23:07:34 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,28 +5,30 @@ import java.net.Socket;
 public class Main {
   public static void main(String[] args) throws IOException {
     System.out.println("Logs from your program will appear here!");
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
     int port = 6379;
-    try {
-      serverSocket = new ServerSocket(port);
-      serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept();
-      while (true) {
-        byte[] input = new byte[1024];
-        clientSocket.getInputStream().read(input);
-        clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
-      }
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
+    final ServerSocket serverSocket = new ServerSocket(port);
+    serverSocket.setReuseAddress(true);
+    Thread thread = new Thread(() -> {
+      Socket clientSocket = null;
       try {
-        if (clientSocket != null) {
-          clientSocket.close();
+        clientSocket = serverSocket.accept();
+        while (true) {
+          byte[] input = new byte[1024];
+          clientSocket.getInputStream().read(input);
+          clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
         }
       } catch (IOException e) {
         System.out.println("IOException: " + e.getMessage());
+      } finally {
+        try {
+          if (clientSocket != null) {
+            clientSocket.close();
+          }
+          serverSocket.close();
+        } catch (IOException e) {
+          System.out.println("IOException: " + e.getMessage());
+        }
       }
-    }
+    });
   }
 }
