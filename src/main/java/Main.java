b7978098b1e4a2a0d7a75bc2c31df6f413657d63
Shashwat Tripathi <shashwattrpth96@gmail.com>
2025-10-02 14:23:57 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -91,17 +91,14 @@ public class Main {
           String data = "*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port" + "\r\n$4\r\n" + Integer.toString(port)
               + "\r\n";
           mout.write(data.getBytes());
-          parseInput(used, buf, masterSock.getInputStream());
           it = parseInput(used, buf, masterSock.getInputStream());
           buf = it.buf;
           used = it.used;
           mout.write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n".getBytes());
-          parseInput(used, buf, masterSock.getInputStream());
           it = parseInput(used, buf, masterSock.getInputStream());
           buf = it.buf;
           used = it.used;
           mout.write("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n".getBytes());
-          parseInput(used, buf, masterSock.getInputStream());
           it = parseInput(used, buf, masterSock.getInputStream());
           buf = it.buf;
           used = it.used;
