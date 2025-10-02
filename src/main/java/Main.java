6c27770c01bd5708687b435f3d428d7eea823e0c
Shashwat Tripathi <shashwattrpth96@gmail.com>
2025-10-02 17:38:38 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -89,6 +89,9 @@ public class Main {
           k = masterSock.getInputStream().read(buf, k, buf.length - k);
           List<String> commands = new ArrayList<>();
           StringBuilder sb = new StringBuilder();
+          for (int i = 0; i < k; i++) {
+            System.out.println(buf[i]);
+          }
           for (int i = 0; i < k;) {
             if (buf[i] == '*' && i + 1 < k && buf[i + 1] >= 48 && buf[i + 1] <= 57) {
               if (sb.length() > 0) {
