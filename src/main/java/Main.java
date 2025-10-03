b9a37fd580ebf8ce325496d0269fb7eb58138826
Shashwat Tripathi <shashwattrpth96@gmail.com>
2025-10-03 16:12:46 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -319,6 +319,7 @@ public class Main {
         }
       }
       pool.shutdownNow();
+      System.out.println("completed wait");
       out.write((":" + count + "\r\n").getBytes());
     } else if (commands.get(0).equalsIgnoreCase("psync")) {
       out.write("+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0\r\n".getBytes());
