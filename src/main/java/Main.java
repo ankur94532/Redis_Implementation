260fece0e21c5d1d22362441b880e87d1d1566a1
Shashwat Tripathi <shashwattrpth96@gmail.com>
2025-10-03 17:16:28 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -317,7 +317,7 @@ public class Main {
         }
       }
       pool.shutdownNow();
-      out.write((":" + count + "\r\n").getBytes());
+      out.write(("*1\r\n:" + count + "\r\n").getBytes(java.nio.charset.StandardCharsets.US_ASCII));
     } else if (commands.get(0).equalsIgnoreCase("psync")) {
       out.write("+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0\r\n".getBytes());
       byte[] str = HexFormat.of().parseHex(
