8e53fc27eb84d6cba1adc4240e1b141c0b491066
Shashwat Tripathi <shashwattrpth96@gmail.com>
2025-10-03 16:14:02 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -321,6 +321,7 @@ public class Main {
       pool.shutdownNow();
       System.out.println("completed wait");
       out.write((":" + count + "\r\n").getBytes());
+      System.out.println("hlo");
     } else if (commands.get(0).equalsIgnoreCase("psync")) {
       out.write("+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0\r\n".getBytes());
       byte[] str = HexFormat.of().parseHex(
